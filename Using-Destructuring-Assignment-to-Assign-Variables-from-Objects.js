d26c59542cadc362d8dbf3051ef7a6390b4bedea
Bybreen Samuels <bybreensamuels@googlemail.com>
2019-06-24 12:58:09 +0100

    Use Destructuring Assignment to Assign Variables from Objects
    Similar to arrays, you can use the spread operator to spread of unpack the contents of an object . The destructuring assignment is special syntax for neatly assigning values taken directly from an object to variables.
    
    E.G ES5 code:
    
    var voxel = {x: 3.6, y: 7.4, z: 6.54 };
    var x = voxel.x; // x = 3.6
    var y = voxel.y; // y = 7.4
    var z = voxel.z; // z = 6.54
    Here's the same assignment statement with ES6 destructuring syntax:
    
    const { x, y, z } = voxel; // x = 3.6, y = 7.4, z = 6.54
    If instead you want to store the values of voxel.x into a, voxel.y into b, and voxel.z into c, you have that freedom as well.
    
    const { x : a, y : b, z : c } = voxel // a = 3.6, b = 7.4, c = 6.54

---
+++ Using-Destructuring-Assignment-to-Assign-Variables-from-Objects.js
@@ -0,0 +1,14 @@
+// Use destructuring to obtain the average temperature for tomorrow from the input object AVG_TEMPERATURES, 
+// and assign value with key tomorrow to tempOfTomorrow in line.
+
+const AVG_TEMPERATURES = {
+today: 77.5;
+tomorrow: 79;
+};
+
+function getTempOFTmrw(avgTemperatures) {
+"use strict";
+
+const { tomorrow : tempOfTomorrow } = avgTemperatures
+
+return tempOfTomorrow;
